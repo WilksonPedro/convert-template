ff3133c55c4b9be3cbd993128c7e50e46174bb45
wilkson pedro da Silva <wil.thydus@gmail.com>
2024-09-18 15:53:03 -0300

    Adicionando constantes para o valor das moedas e capturando elementos do HTML

--- scripts.js
+++ scripts.js
@@ -1,5 +1,10 @@
+// criando constantes para o valor das moedas
+const USD = 5.24;
+const EUR = 6.20;
+const GBP = 7.20;
+
 // capturando os elementos do html
-const form = document.getElementById('form');
+const form = document.querySelector('form');
 const amount = document.getElementById('amount');
 const currency = document.getElementById('currency');
 
@@ -12,7 +17,24 @@ amount.addEventListener("input", () => {
     // excluindo as letras usando "" vazias
 })
 
-currency.onsubmit = (event) => {
+form.onsubmit = (event) => {
     event.preventDefault();
     // para ver o que esta sendo capturado no console usar console.log(currency.value)
+    switch (currency.value) {
+        case "USD":
+            convertCurrency(amount.value, USD, "$");
+            break;
+        case "EUR":
+            convertCurrency(amount.value, EUR, "€");
+            break;
+        case "GBP":
+            convertCurrency(amount.value, GBP, "£");
+            break;
+    }
+}
+
+// função para converter a moeda
+function convertCurrency(amount, price, symbol) {
+    console.log(amount, price, symbol);
+    
 }
